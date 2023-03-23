2efd2037d19bc6ee53d30113adc2acf1e1414611
krishna <krishnareddykurma932@gmail.com>
2023-03-23 06:22:44 +0000

    API 3 IN TWITTER

---
+++ app.js
@@ -0,0 +1,139 @@
+const express = require("express");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+app.use(express.json());
+const dbPath = path.join(__dirname, "twitterClone.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+app.post("/register/", async (request, response) => {
+  let { username, name, password, gender } = request.body;
+
+  let hashedPassword = await bcrypt.hash(password, 10);
+
+  let checkTheUsername = `
+            SELECT *
+            FROM user
+            WHERE username = '${username}';`;
+  let userData = await db.get(checkTheUsername);
+  if (userData === undefined) {
+    let postNewUserQuery = `
+            INSERT INTO
+            user (username,name,password,gender)
+            VALUES (
+                '${username}',
+                '${name}',
+                '${hashedPassword}',
+                '${gender}'
+                
+            );`;
+    if (password.length < 6) {
+      response.status(400);
+      response.send("Password is too short");
+    } else {
+      let newUserDetails = await db.run(postNewUserQuery);
+      response.status(200);
+      response.send("User created successfully");
+    }
+  } else {
+    response.status(400);
+    response.send("User already exists");
+  }
+});
+
+app.post("/login/", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid user");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched === true) {
+      const payload = {
+        username: username,
+      };
+      const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN");
+      response.send({ jwtToken });
+      console.log(jwtToken);
+    } else {
+      response.status(400);
+      response.send("Invalid password");
+    }
+  }
+});
+
+const authenticateToken = (request, response, next) => {
+  let jwtToken;
+  const authHeader = request.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401);
+    response.send("Invalid JWT Token");
+  } else {
+    jwt.verify(jwtToken, "MY_SECRET_TOKEN", async (error, payload) => {
+      if (error) {
+        response.status(401);
+        response.send("Invalid JWT Token");
+      } else {
+        request.username = payload.username;
+        next();
+      }
+    });
+  }
+};
+app.get("/user/tweets/feed/", authenticateToken, async (request, response) => {
+  let { username } = request;
+  const getQuery = `
+    SELECT
+    *
+    FROM
+    user
+    WHERE
+    username="${username}"`;
+  const dbUser = await db.get(getQuery);
+  let UserId = dbUser.user_id;
+  const tweetsQuery = `
+SELECT
+user.username, tweet.tweet, tweet.date_time AS dateTime
+FROM
+follower
+INNER JOIN tweet
+ON follower.following_user_id = tweet.user_id
+INNER JOIN user
+ON tweet.user_id = user.user_id
+WHERE
+follower.follower_user_id = ${UserId}
+ORDER BY
+tweet.date_time DESC
+LIMIT 4;`;
+  const k = await db.all(tweetsQuery);
+  response.send(k);
+});
+
+module.exports = app;
